adef633c5027d565e084df8698b111b273517c2a
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-11-30 17:47:35 +0500

    resolve mongoDB error

--- api/db/connectDb.js
+++ api/db/connectDb.js
@@ -2,16 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URL,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-      () => {
-        console.log("MongoDB Connected!");
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log("Database Connection Error: ", err.message);
   }
--- api/server.js
+++ api/server.js
@@ -1,14 +1,7 @@
 import app from "./index.js";
 import connectDB from "./db/connectDb.js";
 
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+connectDB();
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on PORT: ${process.env.PORT}`);
