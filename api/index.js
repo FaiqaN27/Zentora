9ed1ae7d1cf1530c4a7fe1802238c69b1b4ee486
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-11-30 20:10:24 +0500

    add middlewares

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import ErrorHandler from "./utils/ErrorHandler";
+import ErrorHandler from "./utils/ErrorHandler.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload({ useTempFiles: true }));
 
 app.use(ErrorHandler);
 
